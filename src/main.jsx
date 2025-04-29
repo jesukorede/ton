80904bc03e160fec78ead0b71e1a93d5b4db9093
unknown <benjaminjesukorede@gmail.com>
2025-04-29 17:12:20 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { HashRouter } from 'react-router-dom'
 import { MetaverseProvider } from './components/MetaverseContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-    <MetaverseProvider>
-      <App />
-    </MetaverseProvider>
+      <MetaverseProvider>
+        <App />
+      </MetaverseProvider>
     </HashRouter>
   </React.StrictMode>
 )
