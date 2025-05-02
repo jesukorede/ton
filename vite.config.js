af6a468ad006bd01da21794275ebab99ff824a5a
unknown <benjaminjesukorede@gmail.com>
2025-05-02 11:57:39 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ton',  // Note the trailing slash
+  base: '/ton/',  // Note the trailing slash
   plugins: [react()],
   server: {
     port: 3000,
